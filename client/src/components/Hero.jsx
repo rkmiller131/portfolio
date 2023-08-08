13d7fe27b28be68c93d99a97c352d0a38c060752
Rachel Miller <rkmiller131@gmail.com>
2023-08-07 18:54:30 -0700

    Basic hero background: linear gradient and white grid, hero slant

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Hero() {
+  return (
+    <div className="hero-container">
+      <div className="white-grid"></div>
+      <div className="hero-slant">
+        Test
+      </div>
+    </div>
+  )
+}
