7acc46ac705c0cb99a5da934563c0cc82ed7540b
Rachel Miller <rkmiller131@gmail.com>
2023-12-30 09:48:13 -0800

    Revised listed projects and descriptions. Fixed broken link

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -33,9 +33,10 @@ export default function Projects() {
   const leftProjects = [
     {
       title: 'Paper Cat Games',
-      description: `Conducted in-depth research on the client's brand, voice, and misssion in order to build a Figma wireframe and project proposal. Skillfully managed the project's transition from Node.js to Next.js with React Bricks CMS integration, achieving the stretch goal of enabling the client to independently add and manage website content. Crafted unique layouts with CSS modules for a tailored user experience.`,
+      description: `A promotional website for an indie game studio. Recognized the client's need for autonomous content management without web development expertise, and proposed a bespoke CMS solution using React Bricks that empowered them with the flexibility to make real-time changes and add future content as the studio expands. Leveraged Next.js's dynamic routes to add new pages each time the admin creates a blog post.`,
       src: 'https://res.cloudinary.com/dnr41r1lq/video/upload/v1702160880/PapercatAdminDemo_zaar0r.mp4',
       github: 'https://github.com/Paper-Studios/cms-papercat',
+      link: 'https://admirable-clafoutis-124804.netlify.app/',
       id: 198
     },
     {
@@ -43,21 +44,29 @@ export default function Projects() {
       description: 'Translated UI wireframe design into a SPA, creating an innovative fractional 5-star rating system and product breakdown charts with vanilla CSS. Employed industry standard testing libraries such as Jest and RTL, achieving 80% code coverage for unit tests. Enabled photo uploads in URL format via externally hosted base64 data to address backend compatibility issues.',
       src: 'https://res.cloudinary.com/dnr41r1lq/video/upload/v1692139856/ShortenedFECDemo_vyagyn.mp4',
       github: 'https://github.com/CBC-Ecommerce/styl-forge',
-      link: 'http://54.176.80.166:3000/',
+      link: 'http://54.177.180.141:3000/',
       id: 567
     },
+    // {
+    //   title: 'Village Sports',
+    //   description: 'A sports league meetup mobile app with React Native. Developed a Home Page carousel, weekly announcement feed and Wishlist feature, integrating Firebase authentication and Firestore database.',
+    //   src: 'https://res.cloudinary.com/dnr41r1lq/video/upload/v1692151765/Untitled_video_ohhxpp.mp4',
+    //   github: 'https://github.com/Blue-Ocean-Naruto/Village-Sports',
+    //   id: 345
+    // }
     {
-      title: 'Village Sports',
-      description: 'A sports league meetup mobile app with React Native. Developed a Home Page carousel, weekly announcement feed and Wishlist feature, integrating Firebase authentication and Firestore database.',
-      src: 'https://res.cloudinary.com/dnr41r1lq/video/upload/v1692151765/Untitled_video_ohhxpp.mp4',
-      github: 'https://github.com/Blue-Ocean-Naruto/Village-Sports',
-      id: 345
+      title: 'Dog Age Calculator',
+      description: `A fun app for converting a dog's age into the human equivalent using two different formulas (it's a little more than just times 7). Created over a holiday weekend using an accessible, mobile-first approach.`,
+      src: 'https://res.cloudinary.com/dnr41r1lq/video/upload/v1703956908/dogAgeDemo_vughie.mp4',
+      github: 'https://github.com/rkmiller131/dog-age-calculator',
+      link: 'https://calculatedogage.netlify.app/',
+      id: 7922
     }
   ];
   const rightProjects = [
     {
       title: 'WhimsiWorld',
-      description: 'Pioneered the inception of a collaborative, turn-based creative writing platform for storytelling with friends using multimedia assets, community engagement, and competition. Created over 100 visual assets and story packs using Photoshop, Figma, and Midjourney AI and styled the storyboard home page with React and Tailwind CSS to ensure a cohesive and engaging user experience.',
+      description: 'Pioneered the inception of a collaborative, turn-based creative writing platform for storytelling with friends using multimedia assets, community engagement, and competition. Created over 100 visual assets and story packs using Photoshop, Figma, generative AI, and styled the storyboard home page with React and Tailwind CSS to ensure a cohesive and engaging user experience.',
       src: 'https://res.cloudinary.com/dnr41r1lq/video/upload/v1695477373/WhimsiWorldDemo_mvdoxy.mp4',
       github: 'https://github.com/WhimsWorld/BlueOcean',
       id: 222
@@ -71,7 +80,7 @@ export default function Projects() {
     },
     {
       title: 'LifeTrack Labs',
-      description: 'A minimum viable product (MVP) created in 2.5 days as a prototype for a blood lab interpretation app. A passion project that uses unbiased research to establish correlations between blood lab results and lifestyle choices.',
+      description: 'A minimum viable product (MVP) created in 2.5 days as a prototype for a blood lab interpretation app. A passion project that I want the world to have; an app that uses unbiased research to establish correlations between blood lab results and lifestyle choices. Open to collab? Contact me!',
       src: 'https://res.cloudinary.com/dnr41r1lq/video/upload/v1692147931/LifeTrackDemo_1_oepqcq.mp4',
       github: 'https://github.com/rkmiller131/LifeTrack-Labs',
       id: 789
